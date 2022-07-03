2a3a1324784ac516205ea5f2a228329b15ee6b67
XaVV9 <86689586+XaVV9@users.noreply.github.com>
2022-07-03 21:50:55 +0200

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,7 +2,7 @@ module.exports = async (client) => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setPresence({
     activity: {
-      name: `SEA GEN BOOSTER GEN BY XVXZY2115`,
+      name: `.help | GEN BY XVXZY2115`,
       type: 'WATCHING', //PLAYING, STREAMING, LISTENING, WATCHING, CUSTOM_STATUS
     },
     status: 'online', //online, idle, dnd
