efb726576bb56e87613722969ce1888e6d2bd191
XaVV9 <86689586+XaVV9@users.noreply.github.com>
2022-07-04 20:04:12 +0200

    Update nordvpn.js

--- commands/Generator/nordvpn.js
+++ commands/Generator/nordvpn.js
@@ -59,7 +59,7 @@ module.exports.help = {
   aliases: ['nordvpn', 'nord', 'vpn'],
   category: 'generator',
   description: 'generate a nordvpn account',
-  cooldown: 900,
+  cooldown: 60,
   usage: '',
   example: [],
 };
