377c67fc434b0aa7986eb79712a9c686d575bf3f
XaVV9 <86689586+XaVV9@users.noreply.github.com>
2022-07-04 20:03:31 +0200

    Update minecraft.js

--- commands/Generator/minecraft.js
+++ commands/Generator/minecraft.js
@@ -59,7 +59,7 @@ module.exports.help = {
   aliases: ['minecraft', 'mc'],
   category: 'generator',
   description: 'generate a minecraft account',
-  cooldown: 900,
+  cooldown: 60,
   usage: '',
   example: [],
 };
