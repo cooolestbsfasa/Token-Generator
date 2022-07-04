be6be935458a562533882e0be0abb224ccc66363
XaVV9 <86689586+XaVV9@users.noreply.github.com>
2022-07-04 20:05:33 +0200

    Update uplay.js

--- commands/Generator/uplay.js
+++ commands/Generator/uplay.js
@@ -59,7 +59,7 @@ module.exports.help = {
   aliases: ['uplay', 'Uplay', 'UPLAY'],
   category: 'generator',
   description: 'generate a uplay account',
-  cooldown: 900,
+  cooldown: 60,
   usage: '',
   example: [],
 };
